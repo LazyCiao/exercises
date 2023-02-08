856caafe379d3d3577b2392cd36d02bd1d551cb7
LazyCiao <123471880+LazyCiao@users.noreply.github.com>
2023-02-08 16:43:28 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+list = [10, 20, 33, 46, 55]
+
+# if number is divisible by 5 from the list display it
+for i in list:
+    if i % 5 == 0:
+        print(i)
+    else:
+        print('This number is not divisible by 5')
